6bdfa67527f236c7cbd0f6bc2cf4fc017c630325
Roze <rozenn.paris@gmail.com>
2018-03-30 15:18:06 +0200

    ticketAction addons, number of tickets in step one

--- src/AppBundle/Controller/BookingController.php
+++ src/AppBundle/Controller/BookingController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Booking;
 use AppBundle\Entity\Ticket;
 use AppBundle\Form\BookingType;
+use AppBundle\Form\TicketsType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,13 +26,20 @@ class BookingController extends AbstractController
         $bookingForm = $this->createForm(BookingType::class, $booking);
 
         $bookingForm->handleRequest($request);
+
         if($bookingForm->isSubmitted() && $bookingForm->isValid())
         {
+            // préparer le nombre de tickets
+            for ($i = 1; $i <= $booking->getNumberOfTickets(); $i++)
+            {
+                $booking->addTicket(new Ticket());
+            }
             //sauvegarder en session booking
-            $request->getSession();
+            $this->get('session')->set('booking', $booking);
+
 
             // rediriger vers step2
-            return $this->redirectToRoute('Booking/ticket.html.twig');
+            return $this->redirectToRoute('ticket');
 
         }
 
@@ -41,5 +49,52 @@ class BookingController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/ticket", name="ticket")
+     * @Method({"GET","POST"})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function ticketAction(Request $request, SessionInterface $session)
+    {
+        /**
+         * @var Booking $booking
+         */
+        $booking = $session->get('booking'); //gérer le cas où pas de booking
 
+        if (!$booking)
+        {
+            throw $this->createNotFoundException("La commande n'a pas été initialisée");
+        }
+
+        $ticketForm = $this->createForm(TicketsType::class, $booking);
+        $ticketForm->handleRequest($request);
+
+        if($ticketForm->isSubmitted() && $ticketForm->isValid())
+        {
+                //enregistrement en session
+                $this->get('session')->set('ticket', $booking);
+                echo '<pre>'; var_dump($booking->getTickets()); die;
+
+                //go to step3
+                return $this->redirectToRoute('summary');
+        }
+
+        return $this->render('Booking/ticket.html.twig', [
+                'form'=>$ticketForm->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/summary", name="summary")
+     * @Method({"GET","POST"})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function summaryAction(Request $request, SessionInterface $session)
+    {
+
+    }
 }
+
+
